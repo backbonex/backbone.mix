6135e0afef8ae4cfa032cbf3d01addae67dbf54d
Andrey Balakirev <andrey.balakirev@alpari.org>
2013-06-17 13:16:15 +0300

    PSDJS-5351 hide dropdown on touch

--- Backbone.Mix.js
+++ Backbone.Mix.js
@@ -16,15 +16,37 @@
  * </code>
  */
 (function (Backbone) {
+    /**
+     * @class Backbone.Mixin
+     * @constructor
+     * @param {Object} [options]
+     * @param {Array.<Object|Backbone.Mixin>} [options.dependencies]
+     * @param {Object} proto
+     */
+    var Mixin = Backbone.Mixin = function (options, proto) {
+        if (!proto) {
+            proto = options;
+            options = {};
+        }
+
+        this.proto = proto;
+        this.dependencies = options.dependencies || [];
+    };
+
     var checkForSuper = /\b_super\b/;
 
     /**
      * @returns {Function}
      */
-    var mix = function (/**{Object} mixin1, ..*/) {
+    var mix = function (/**{Object|Backbone.Mixin} mixin1, ..*/) {
         var Class = this;
 
         _(arguments).forEach(function (mixin) {
+            if (mixin instanceof Mixin) {
+                Class = mix.apply(Class, mixin.dependencies);
+                mixin = mixin.proto;
+            }
+
             if (mixin.initialize && !checkForSuper.test(mixin.initialize.toString())) {
                 var init = mixin.initialize;
                 mixin.initialize = function () {
